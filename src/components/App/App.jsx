35765c4d9fe6babf1bb21a5875c065acb0e89cac
Oleksandr Kutsenko <104622826+Fstv-Electro@users.noreply.github.com>
2023-02-14 15:17:01 +0200

    maybe ready/ can't check

---
+++ src/components/App/App.jsx
@@ -0,0 +1,67 @@
+import React, { Components } from 'react';
+import { nanoid } from 'nanoid';
+
+import { Container, Title, Text } from './App.styled';
+import ContactForm from 'components/ContactForm/ContactForm';
+import ContactList from 'components/Contacts/ContactList';
+import Filter from 'components/Filter';
+
+
+class App extends Components {
+
+  state = {
+  contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  ],
+  filter: '',
+}
+
+  handleSubmit = data => {
+    const sameName = this.state.contacts.find(item => item.name.toLowerCase() === data.name.toLowerCase());
+    if (sameName) return alert(sameName.name + ' already in your contacts');
+
+    data.id = nanoid();
+    this.setState( prev => ({ contacts: [data, ...prev.contacts] }));
+  };
+  
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getVisibleContacts = () => {
+    const { filter, contacts } = this.state;
+    const lowerFilter = filter.toLowerCase();
+
+    return contacts.filter(item => item.name.toLowerCase().includes(lowerFilter));
+  };
+
+  deleteContacts = id => {
+    this.setState(prevState => ({ contacts: prevState.contacts.filter(item => item.id !== id), }))
+  };
+  
+  render() {
+    const { filter, contacts } = this.state;
+
+    return (
+      <Container>
+        <Title>Phonebook</Title>
+        <ContactForm onSubmit={this.handleSubmit} />
+        <Title>Contacts</Title>
+        <Text>Find contact</Text>
+        <Filter value={filter} onChange={this.changeFilter} />
+        {contacts.length ?
+          (
+            <ContactList contacts={this.getVisibleContacts()} onDelete={this.deleteContacts} />
+          ) : (
+        <Text>You havn't contacts yet</Text>
+        )}
+        
+      </Container>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Contacts/ContactList.styled.jsx
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled";
+
+export const List = styled.ul`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 8px;
+    padding: 16px;
+    list-style: none;
+`
+
+export const ListItem = styled.li`
+    display: flex;
+    align-items: center;
+    gap: 32px;
+`
+
+export const Button = styled.button`
+    padding: 16px;
+    border: 1px solid #2a2a2a;
+    border-radius: 4px;
+`
